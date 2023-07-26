ea504f7c64d0643a7b4948d40a31a95a7347985f
RenzVillegas24 <renzjustinevillegas@gmail.com>
2023-07-26 22:46:44 +0800

    Marami pa ring Bugs v1.0b

--- src/main.cpp
+++ src/main.cpp
@@ -223,65 +223,59 @@ int drawMenu(String header,
       iSz = items.size();
   while (true)
   {
-
     int itmLen = items[selectedY].length(),
         btnEvent = btnClick(),
         itmPx = (itmLen * 5) + itmLen - 1 + 16,
         selL = (SCREEN_WIDTH - (itmPx)) / 2;
 
-    display.clearDisplay();
-    display.setTextColor(SSD1306_WHITE);
-    drawStr(header, 0, 7, align);
+    if (btnEvent == 0){
+      display.clearDisplay();
+      display.setTextColor(SSD1306_WHITE);
+      drawStr(header, 0, 7, align);
 
-    if (hasBack)
-    {
-      drawArrow(LEFT, 11, 10, 3, SSD1306_WHITE);
-      if (selectedY == -1)
-        display.drawRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
-    }
-
-    if (selectedY != -1)
-      display.drawRoundRect(selL, 21 + 12 * selectedY, itmPx, 13, 4, SSD1306_WHITE);
-
-    for (int i = 0; i < iSz; i++)
-      drawStr(items[i], 0, 24 + (12 * i), align);
-
-    display.display();
-
-    while (btnClick() != 0)
-    {
-      btnEvent = btnClick();
-
-      if (btnEvent == 1)
+      if (hasBack)
       {
-        if (selectedY == -1 || selectedY == iSz)
-        {
-          display.fillRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
-          drawArrow(LEFT, 11, 10, 3, SSD1306_BLACK);
-        }
-        else
-        {
-          display.fillRoundRect(selL, 21 + 12 * selectedY, itmPx, 13, 4, SSD1306_WHITE);
-          display.setTextColor(SSD1306_BLACK);
-          drawStr(items[selectedY], 0, 24 + (12 * selectedY), align);
-        }
-        display.display();
+        drawArrow(LEFT, 11, 10, 3, SSD1306_WHITE);
+        if (selectedY == -1)
+          display.drawRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
+      }
 
-        wait();
-        delay(100);
-        return selectedY;
+      if (selectedY != -1)
+        display.drawRoundRect(selL, 21 + 12 * selectedY, itmPx, 13, 4, SSD1306_WHITE);
+
+      for (int i = 0; i < iSz; i++)
+        drawStr(items[i], 0, 24 + (12 * i), align);
+
+      display.display();
+      wait(1);
+    }
+    else if (btnEvent == 1)
+    {
+      if (selectedY == -1 || selectedY == iSz)
+      {
+        display.fillRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
+        drawArrow(LEFT, 11, 10, 3, SSD1306_BLACK);
       }
       else
       {
-        if (btnEvent == TOP)
-          selectedY--; // top
-        else if (btnEvent == BOTTOM)
-          selectedY++; // bottom
-        wait();
-        break;
+        display.fillRoundRect(selL, 21 + 12 * selectedY, itmPx, 13, 4, SSD1306_WHITE);
+        display.setTextColor(SSD1306_BLACK);
+        drawStr(items[selectedY], 0, 24 + (12 * selectedY), align);
       }
-    }
+      display.display();
 
+      wait(0);
+      delay(100);
+      return selectedY;
+    } else {
+      if (btnEvent == TOP)
+        selectedY--; // top
+      else if (btnEvent == BOTTOM)
+        selectedY++; // bottom
+
+      wait(0);
+    }
+    
     if (hasBack)
     {
       if (selectedY == -2)
@@ -296,6 +290,7 @@ int drawMenu(String header,
       else if (selectedY == iSz)
         selectedY %= iSz;
     }
+
   }
 }
 
@@ -309,29 +304,6 @@ int drawDialog(String header,
       iSz = items.size(),
       mSz = message.size();
 
-  while (true)
-  {
-    int btnEvent = btnClick(),
-        itmLen = items[selectedX].length(),
-        itmPx = (itmLen * 5) + itmLen - 1 + 16,
-        selL = (SCREEN_WIDTH - (itmPx)) / 2;
-
-    display.clearDisplay();
-    display.setTextColor(SSD1306_WHITE);
-    drawStr(header, 0, 7, 1);
-
-    for (int i = 0; i < mSz; i++)
-      drawStr(message[i], 0, 21 + (10 * i), align);
-    
-
-
-    if (hasBack)
-    {
-      drawArrow(LEFT, 11, 10, 3, SSD1306_WHITE);
-      if (selectedX == -1)
-        display.drawRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
-    }
-
     int xStart = 0;
     for (int i = 0; i < iSz; i++)
     {
@@ -341,60 +313,80 @@ int drawDialog(String header,
     }
     xStart = (SCREEN_WIDTH - (xStart)) / 2;
 
-    int curX = xStart;
-    for (int i = 0; i < iSz; i++)
+    int curX;
+  while (true)
+  {
+    int btnEvent = btnClick(),
+        itmLen = items[selectedX].length(),
+        itmPx = (itmLen * 5) + itmLen - 1 + 16,
+        selL = (SCREEN_WIDTH - (itmPx)) / 2;
+
+    if (btnEvent == 0)
     {
-      int iLen = items[i].length(),
-          iPx = (iLen * 5) + iLen - 1 + 16;
+      display.clearDisplay();
+      display.setTextColor(SSD1306_WHITE);
+      drawStr(header, 0, 7, 1);
 
-      drawStr(items[i], curX + 8, 48, 0);
+      for (int i = 0; i < mSz; i++)
+        drawStr(message[i], 0, 21 + (10 * i), align);
 
-      if (selectedX == i)
-        display.drawRoundRect(curX, 45, iPx, 13, 4, SSD1306_WHITE);
-
-      curX += iPx;
-    }
-
-    display.display();
-    while (btnClick() != 0)
-    {
-      btnEvent = btnClick();
-
-      if (btnEvent == 1)
+      if (hasBack)
       {
-        if (selectedX == -1 || selectedX == iSz)
-        {
-          display.fillRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
-          drawArrow(LEFT, 11, 10, 3, SSD1306_BLACK);
-        }
-        else
-        {
-          curX = xStart;
-          for (int i = 0; i < selectedX; i++)
-          {
-            int iLen = items[i].length();
-            curX += (iLen * 5) + iLen - 1 + 16;
-          }
+        drawArrow(LEFT, 11, 10, 3, SSD1306_WHITE);
+        if (selectedX == -1)
+          display.drawRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
+      }
+      curX = xStart;
+      for (int i = 0; i < iSz; i++)
+      {
+        int iLen = items[i].length(),
+            iPx = (iLen * 5) + iLen - 1 + 16;
 
-          display.fillRoundRect(curX, 45, itmPx, 13, 4, SSD1306_WHITE);
-          display.setTextColor(SSD1306_BLACK);
-          drawStr(items[selectedX], curX + 8, 48, 0);
-        }
-        display.display();
+        drawStr(items[i], curX + 8, 48, 0);
 
-        wait();
-        delay(100);
-        return selectedX;
+        if (selectedX == i)
+          display.drawRoundRect(curX, 45, iPx, 13, 4, SSD1306_WHITE);
+
+        curX += iPx;
+      }
+
+      display.display();
+      wait(1);
+    }
+    else if (btnEvent == 1)
+    {
+      if (selectedX == -1 || selectedX == iSz)
+      {
+        display.fillRoundRect(5, 4, 17, 13, 4, SSD1306_WHITE);
+        drawArrow(LEFT, 11, 10, 3, SSD1306_BLACK);
       }
       else
       {
-        if (btnEvent == LEFT)
-          selectedX--; // left
-        else if (btnEvent == RIGHT)
-          selectedX++; // right
-        wait();
-        break;
+        curX = xStart;
+        for (int i = 0; i < selectedX; i++)
+        {
+          int iLen = items[i].length();
+          curX += (iLen * 5) + iLen - 1 + 16;
+        }
+
+        display.fillRoundRect(curX, 45, itmPx, 13, 4, SSD1306_WHITE);
+        display.setTextColor(SSD1306_BLACK);
+        drawStr(items[selectedX], curX + 8, 48, 0);
       }
+      display.display();
+
+      wait(0);
+      delay(100);
+      return selectedX;
+    }
+    else
+    {
+      if (btnEvent == LEFT)
+        selectedX--; // left
+      else if (btnEvent == RIGHT)
+        selectedX++; // right
+      wait(0);
+      break;
     }
 
     if (hasBack)
@@ -507,7 +499,7 @@ int drawSetting(String header,
           display.drawLine(curLnL - 1, 44, curLnR , 44, SSD1306_BLACK);
 
           display.display();
-          wait();
+          wait(0);
           delay(100);
           return selectedX;
         }
@@ -517,7 +509,7 @@ int drawSetting(String header,
           drawArrow(LEFT, 11, 10, 3, SSD1306_BLACK);
 
           display.display();
-          wait();
+          wait(0);
           delay(100);
           return -1;
         }
@@ -562,7 +554,7 @@ int drawSetting(String header,
           }
 
           display.display();
-          wait();
+          wait(0);
           delay(100);
 
           if (btnEvent == LEFT)
@@ -576,7 +568,7 @@ int drawSetting(String header,
         else if (btnEvent == BOTTOM)
           selectedY++; // bottom
 
-        wait();
+        wait(0);
         break;
       }
     }
